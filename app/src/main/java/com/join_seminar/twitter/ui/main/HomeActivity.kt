3bd2da404351f2b9c3a62b85ce546c757fa2fe3d
lhb8106 <lhb8106@naver.com>
2022-05-17 21:22:33 +0900

    [CHORE] #1 - 패키징 추가

--- app/src/main/java/com/join_seminar/twitter/ui/main/HomeActivity.kt
+++ app/src/main/java/com/join_seminar/twitter/ui/main/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.join_seminar.twitter
+package com.join_seminar.twitter.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.join_seminar.twitter.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
---
+++ app/src/main/java/com/join_seminar/twitter/ui/write/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.join_seminar.twitter.ui.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.join_seminar.twitter.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}

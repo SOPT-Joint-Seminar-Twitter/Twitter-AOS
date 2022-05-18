0584c4636d017b63c155692ca0d5f7b1123e6616
lhb8106 <lhb8106@naver.com>
2022-05-18 15:22:15 +0900

    [FEAT] #4 - viewpager 및 메인 부분 뷰 완료

--- app/src/main/java/com/join_seminar/twitter/ui/main/HomeActivity.kt
+++ app/src/main/java/com/join_seminar/twitter/ui/main/HomeActivity.kt
@@ -2,11 +2,40 @@ package com.join_seminar.twitter.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import com.join_seminar.twitter.R
+import com.join_seminar.twitter.databinding.ActivityHomeBinding
+import com.sopt.anroid_hyebin.util.BaseActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+
+    private lateinit var homeTabAdapter: HomeTabAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        initAdapter()
+        initTab()
+
     }
+
+
+    private fun initAdapter() {
+        val fragmentList = listOf(InfoFragment(), TwitFragment(),ReplyFragment(),MediaFragment(), MindFragment())
+        homeTabAdapter = HomeTabAdapter(this)
+        homeTabAdapter.fragments.addAll(fragmentList)
+
+
+        binding.vpHome.adapter = homeTabAdapter
+
+    }
+
+    private fun initTab(){
+        val tabLabel = listOf("정보", "트윗", "트윗과답글", "미디어", "마음에 들어요")
+        TabLayoutMediator(binding.tabHome, binding.vpHome) {tab, position ->
+            tab.text = tabLabel[position]
+        }.attach()
+    }
+
+
 }

3fa80bdcd2e6737f5acb696d3b7d7cea203950c4
lhb8106 <lhb8106@naver.com>
2022-05-18 16:34:13 +0900

    [FEAT] #4 - recyclerview 추가

---
+++ app/src/main/java/com/join_seminar/twitter/ui/main/TwitAdapter.kt
@@ -0,0 +1,32 @@
+package com.join_seminar.twitter.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.join_seminar.twitter.data.TwitData
+import com.join_seminar.twitter.databinding.ItemTwitListBinding
+
+class TwitAdapter : RecyclerView.Adapter<TwitAdapter.TwitViewHolder>() {
+    val twitList = mutableListOf<TwitData>()
+
+    class TwitViewHolder(val binding : ItemTwitListBinding) : RecyclerView.ViewHolder(binding.root){
+        fun onBind(data: TwitData) {
+           binding.apply {
+               twit = data
+               executePendingBindings()
+           }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TwitViewHolder {
+        val binding = ItemTwitListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return TwitViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TwitViewHolder, position: Int) {
+        holder.onBind(twitList[position])
+    }
+
+    override fun getItemCount(): Int = twitList.size
+}

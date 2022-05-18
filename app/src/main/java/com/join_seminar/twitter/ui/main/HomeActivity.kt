9b3963e4c3fcc2660b4479e3cbd92b3e39155c0f
김현지 <>
2022-05-19 03:31:59 +0900

    [FEAT] #5 - TabLayout 구현

--- app/src/main/java/com/join_seminar/twitter/ui/main/HomeActivity.kt
+++ app/src/main/java/com/join_seminar/twitter/ui/main/HomeActivity.kt
@@ -2,11 +2,37 @@ package com.join_seminar.twitter.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import com.join_seminar.twitter.R
+import com.join_seminar.twitter.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+    private lateinit var tabViewPagerAdapter : TabViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAdapter()
+        initTabLayout()
+    }
+
+    private fun initAdapter(){
+        val fragmentList = listOf(InfoFragment(),TwitFragment(),ReplyFragment(),MediaFragment(),FancyFragment())
+
+        tabViewPagerAdapter = TabViewPagerAdapter(this)
+        tabViewPagerAdapter.fragments.addAll(fragmentList)
+
+        binding.vpHomeTab.adapter= tabViewPagerAdapter
+    }
+
+    private fun initTabLayout(){
+        val tabLabel = resources.getStringArray(R.array.tap_list)
+
+        TabLayoutMediator(binding.tlHomeTab, binding.vpHomeTab) { tab, position ->
+            tab.text = tabLabel[position]
+        }.attach()
     }
 }
